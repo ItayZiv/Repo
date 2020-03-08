c5a24176c15ca8dc17ccdc8e98f1550e6bebe349
Itay Ziv <itay220204@gmail.com>
2020-03-08 21:39:25 +0200

    Add javadoc to DifferentialDrive

--- src/main/java/frc/megiddolions/lib/control/drivetrain/DifferentialDrive.java
+++ src/main/java/frc/megiddolions/lib/control/drivetrain/DifferentialDrive.java
@@ -7,18 +7,42 @@ public class DifferentialDrive implements DriveTrain{
 
     private final DriveTrainConsumer consumer;
 
+    /**
+     * Creates a DifferentialDrive object with the given output consumer
+     *
+     * @param consumer The consumer to drive the robot with
+     */
     public DifferentialDrive(DriveTrainConsumer consumer) {
         this.consumer = consumer;
     }
 
+    /**
+     * Outputs a left and right power to each side of the consumer
+     *
+     * @param leftPower the power to give the left side along the X axis [-1.0..1.0]. Forward is positive.
+     * @param rightPower the power to give the right side along the X axis [-1.0..1.0]. Forward is positive.
+     */
     public void tankDrive(double leftPower, double rightPower) {
         consumer.drive(leftPower, rightPower);
     }
 
+    /**
+     * Outputs a left and right power to each side of the consumer, based on a voltage.
+     * To achieve proper behaviour a output of 1 is the battery voltage
+     *
+     * @param leftVolts the voltage to give the left side along the X. The range is the battery voltage. Forward is positive.
+     * @param rightVolts the voltage to give the right side along the X. The range is the battery voltage. Forward is positive.
+     */
     public void tankDriveVolts(double leftVolts, double rightVolts) {
         consumer.drive(leftVolts / RobotController.getBatteryVoltage(), rightVolts / RobotController.getBatteryVoltage());
     }
 
+    /**
+     * Outputs a power to each side of the robot based on a given speed and rotation in a arcade drive fashion.
+     *
+     * @param xSpeed the speed to move on the X axis [-1.0..1.0]. Forward positive
+     * @param zRotation the rotation to give around the Z axis [-1.0..1.0]. Clockwise positive
+     */
     public void arcadeDrive(double xSpeed, double zRotation) {
         double leftMotorOutput;
         double rightMotorOutput;
@@ -49,6 +73,10 @@ public class DifferentialDrive implements DriveTrain{
                 MathUtil.clamp(rightMotorOutput, -1.0, 1.0));
     }
 
+
+    /**
+     * Stops the robot
+     */
     @Override
     public void stop() {
         consumer.drive(0, 0);
