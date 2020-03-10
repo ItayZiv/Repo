9afb89b9e6e6275ba19230ff0aa719526f848262
Itay Ziv <itay220204@gmail.com>
2020-03-10 17:16:46 +0200

    prepare for branch and tag

--- src/main/java/frc/megiddolions/auto/paths/InfiniteRecharge.java
+++ src/main/java/frc/megiddolions/auto/paths/InfiniteRecharge.java
@@ -8,7 +8,4 @@ import java.util.List;
 
 public class InfiniteRecharge {
 
-    public List<Pose2d> simpleAutoBack = List.of(new Pose2d(new Translation2d(3.256, -2.386), Rotation2d.fromDegrees(0)),
-            new Pose2d(new Translation2d(5.562, -0.719), Rotation2d.fromDegrees(0)),
-            new Pose2d(new Translation2d()));
 }
