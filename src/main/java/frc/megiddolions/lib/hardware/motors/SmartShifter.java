e069db4695d366a5f440f76c13de64ed4cf42927
Itay Ziv <itay220204@gmail.com>
2020-03-14 18:10:53 +0200

    Update SmartShifter to use DifferentialDriveWheelSpeeds, and to take turning into account

--- src/main/java/frc/megiddolions/lib/hardware/motors/SmartShifter.java
+++ src/main/java/frc/megiddolions/lib/hardware/motors/SmartShifter.java
@@ -1,25 +1,30 @@
 package frc.megiddolions.lib.hardware.motors;
 
 import edu.wpi.first.wpilibj.LinearFilter;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
 
 public class SmartShifter extends Shifter {
     private final double kMaxVelocityLowGear;
-    private LinearFilter speedFilter = LinearFilter.movingAverage(15);
+    private final double kTurningThreshold;
+    private LinearFilter velocityFilter = LinearFilter.movingAverage(15);
 
     public SmartShifter(int port, double lowGearRatio, double highGearRatio, double maxVelocityLowGear) {
-        this(port, lowGearRatio, highGearRatio, 0, maxVelocityLowGear);
+        this(port, lowGearRatio, highGearRatio, 0, maxVelocityLowGear, 1);
     }
 
     public SmartShifter(int port, double lowGearRatio, double highGearRatio, double unitsPerOutputRevolution,
-                        double maxVelocityLowGear) {
+                        double maxVelocityLowGear, double turningThreshold) {
         super(port, lowGearRatio, highGearRatio, unitsPerOutputRevolution);
         this.kMaxVelocityLowGear = maxVelocityLowGear;
+        this.kTurningThreshold = turningThreshold;
     }
 
-    public void update(double velocity) {
-        double filteredVelocity = speedFilter.calculate(Math.abs(velocity));
+    public void update(DifferentialDriveWheelSpeeds velocity) {
+        double filteredVelocity = velocityFilter.calculate(Math.min(Math.abs(velocity.leftMetersPerSecond),
+                Math.abs(velocity.rightMetersPerSecond)));
+        double turningDifference = Math.abs(velocity.leftMetersPerSecond - velocity.rightMetersPerSecond);
 
-        if (filteredVelocity >= kMaxVelocityLowGear)
+        if (filteredVelocity >= kMaxVelocityLowGear && kTurningThreshold <= turningDifference)
             setState(ShifterState.High);
         else
             setState(ShifterState.Low);
